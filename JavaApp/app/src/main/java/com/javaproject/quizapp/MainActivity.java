4f87e35b4502caa3f38e1558efe93a1ce8d81805
OTTO Dieu-Puissant Cyprien <cyprienotto@outlook.fr>
2023-04-14 00:06:39 +0200

    display the first question on load

--- JavaApp/app/src/main/java/com/javaproject/quizapp/MainActivity.java
+++ JavaApp/app/src/main/java/com/javaproject/quizapp/MainActivity.java
@@ -2,15 +2,19 @@ package com.javaproject.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.javaproject.quizapp.databinding.ActivityMainBinding;
 import com.javaproject.quizapp.model.Question;
+import com.javaproject.quizapp.model.QuestionsList;
 import com.javaproject.quizapp.viewmodel.QuizViewModel;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding activityMainBinding;
@@ -19,11 +23,12 @@ public class MainActivity extends AppCompatActivity {
 
     static int result = 0;
     static int totalQuestions = 0;
-
+    List<Question> questionsList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        questionsList = new ArrayList<>();
 
         activityMainBinding = DataBindingUtil
                 .setContentView(this, R.layout.activity_main);
@@ -35,5 +40,22 @@ public class MainActivity extends AppCompatActivity {
         // getting the response:
         quizViewModel = new ViewModelProvider(this)
                 .get(QuizViewModel.class);
+
+
+        // Displaying the First Question
+        quizViewModel.getQuestionsListLiveData().observe(this, new Observer<QuestionsList>() {
+            @Override
+            public void onChanged(QuestionsList questions) {
+                questionsList  = questions;
+
+                Log.i("TAGY","The first Question: "+questions.get(0));
+
+                activityMainBinding.txtQuestion.setText("Question 1 :  " +questions.get(0).getQuestion());
+                activityMainBinding.radio1.setText(questions.get(0).getOption1());
+                activityMainBinding.radio2.setText(questions.get(0).getOption2());
+                activityMainBinding.radio3.setText(questions.get(0).getOption3());
+                activityMainBinding.radio4.setText(questions.get(0).getOption4());
+            }
+        });
     }
 }
