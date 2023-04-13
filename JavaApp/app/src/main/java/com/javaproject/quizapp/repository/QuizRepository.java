83b210bc02bb6cfd088b69defd52dadcc0bda0ce
OTTO Dieu-Puissant Cyprien <cyprienotto@outlook.fr>
2023-04-13 23:35:43 +0200

    api / service interface, retrofit instance

---
+++ JavaApp/app/src/main/java/com/javaproject/quizapp/repository/QuizRepository.java
@@ -0,0 +1,46 @@
+package com.javaproject.quizapp.repository;
+
+import android.util.Log;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.javaproject.quizapp.model.QuestionsList;
+import com.javaproject.quizapp.retrofit.QuestionsAPI;
+import com.javaproject.quizapp.retrofit.RetrofitInstance;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class QuizRepository {
+
+    private QuestionsAPI questionsAPI;
+
+    public QuizRepository() {
+        questionsAPI = new RetrofitInstance()
+                .getRetrofitInstance()
+                .create(QuestionsAPI.class);
+    }
+
+    public LiveData<QuestionsList> getQuestionsFromAPI(){
+
+        MutableLiveData<QuestionsList> data = new MutableLiveData<>();
+        Call<QuestionsList> response = questionsAPI.getQuestions();
+        response.enqueue(new Callback<QuestionsList>() {
+            @Override
+            public void onResponse(Call<QuestionsList> call, Response<QuestionsList> response) {
+                //saving the data to the list
+                QuestionsList list = response.body();
+                data.setValue(list);
+                Log.i("TAGY", "response "+data.getValue());
+            }
+
+            @Override
+            public void onFailure(Call<QuestionsList> call, Throwable t) {
+
+            }
+        });
+        return data;
+
+    }
+}
---
+++ JavaApp/app/src/main/java/com/javaproject/quizapp/retrofit/QuestionsAPI.java
@@ -0,0 +1,12 @@
+package com.javaproject.quizapp.retrofit;
+
+import com.javaproject.quizapp.model.QuestionsList;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.http.GET;
+
+public interface QuestionsAPI {
+    // end point
+    @GET("questionsapi.php")
+    Call<QuestionsList> getQuestions();
+}
