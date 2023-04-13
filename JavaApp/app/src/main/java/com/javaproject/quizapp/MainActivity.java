718b2558f4c39f7ff8a64dbca6f8172a5b72d6cf
OTTO Dieu-Puissant Cyprien <cyprienotto@outlook.fr>
2023-04-13 23:58:50 +0200

    view design & main activity with init binding

--- JavaApp/app/src/main/java/com/javaproject/quizapp/MainActivity.java
+++ JavaApp/app/src/main/java/com/javaproject/quizapp/MainActivity.java
@@ -1,14 +1,29 @@
 package com.javaproject.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.javaproject.quizapp.databinding.ActivityMainBinding;
+import com.javaproject.quizapp.model.Question;
+import com.javaproject.quizapp.viewmodel.QuizViewModel;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding activityMainBinding;
+    QuizViewModel quizViewModel;
+    List<Question> questionList;
+
+    static int result = 0;
+    static int totalQuestion = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
