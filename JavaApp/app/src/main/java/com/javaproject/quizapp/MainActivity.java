8dd2dcbaba84630eb8d56c056ff4ebf322873a9d
OTTO Dieu-Puissant Cyprien <cyprienotto@outlook.fr>
2023-04-14 00:01:25 +0200

    Resetting the Scores & getting the response

--- JavaApp/app/src/main/java/com/javaproject/quizapp/MainActivity.java
+++ JavaApp/app/src/main/java/com/javaproject/quizapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.javaproject.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
@@ -17,13 +18,22 @@ public class MainActivity extends AppCompatActivity {
     List<Question> questionList;
 
     static int result = 0;
-    static int totalQuestion = 0;
+    static int totalQuestions = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding = DataBindingUtil
+                .setContentView(this, R.layout.activity_main);
+
+        // Resetting the Scores:
+        result = 0;
+        totalQuestions = 0;
+
+        // getting the response:
+        quizViewModel = new ViewModelProvider(this)
+                .get(QuizViewModel.class);
     }
 }
